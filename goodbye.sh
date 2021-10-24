321ef4a41fc0258779ff085b13c9931769ee1a13
Robert S Lee <sangkyulee@gmail.com>
2021-10-24 11:52:40 -0400

    add status

--- goodbye.sh
+++ goodbye.sh
@@ -1,3 +1,4 @@
 echo "Goodbye"
 echo "::set-output name=obj-id::$(echo $RANDOM)"
 echo "::set-output name=obj-date::$(date +'%y%m%d-%H%M%S')"
+echo "::set-output name=obj-status::$(ls | paste -s -d, -)"
