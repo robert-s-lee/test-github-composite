5bcca45ad9b49cc312722071373c4ff300b9c7b7
Robert S Lee <sangkyulee@gmail.com>
2021-10-24 14:06:43 -0400

    Update goodbye.sh

--- goodbye.sh
+++ goodbye.sh
@@ -5,8 +5,8 @@ echo "::set-output name=obj-id::$(echo $RANDOM)"
 echo "::set-output name=obj-date::$(date +'%y%m%d-%H%M%S')"
 echo "::set-output name=obj-status::$(ls | paste -s -d, -)"
 
-echo "::set-output name=obj_summary::$(echo $RANDOM)"
+echo "::set-output name=obj_id::$(echo $RANDOM)"
 echo "::set-output name=obj_status::$(date +'%y%m%d-%H%M%S')"
-echo "::set-output name=obj_exit_code::$(ls | paste -s -d, -)"
+echo "::set-output name=obj_summary::$(ls | paste -s -d, -)"
 
 exit 0
